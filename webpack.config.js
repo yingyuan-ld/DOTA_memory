7a38cb452c525ac3b0c7b4ec6b4e54d5ba0c36a1
ludi <ludi@beisen.com>
2019-02-23 17:54:29 +0800

    css拆出，更新readme，css压缩

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,9 @@
-// const webpack = require('webpack')
-// const path = require('path');
 // const HtmlWebpackPlugin = require('html-webpack-plugin');//用于自动生成html入口文件的插件
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");//将CSS代码提取为独立文件的插件
-// const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");//CSS模块资源优化插件
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");//CSS模块资源优化插件
 module.exports = {
     mode:'development',
+    // mode:'production',
     entry: [
         './index.js'
     ],
@@ -21,21 +20,28 @@ module.exports = {
                 test: /\.scss$/,
                 exclude: /node_modules/, //排除node_modules文件夹
                 use: [{
-                     loader: 'style-loader'//建议生产环境采用此方式解耦CSS文件与js文件
-                  },{
-                    loader: 'css-loader',//CSS加载器 
-                  },{
-                    loader: 'sass-loader'//SCSS加载器，webpack默认使用node-sass进行编译
-                  }
+                        loader: MiniCssExtractPlugin.loader//建议生产环境采用此方式解耦CSS文件与js文件
+                    },{
+                        loader: 'css-loader',//CSS加载器 
+                        // options: {modules:true} //CSS代码中的样式名替换为哈希值
+                    },{
+                        loader: 'sass-loader'//SCSS加载器，webpack默认使用node-sass进行编译
+                    }
                 ]
             },
             {test: /\.(png|jpg)$/,loader: 'url-loader?limit=8192'}
         ]
-        // loaders: [                               //exclude 不打包的目录         
-        //     {test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/, query: {presets: ['react', 'es2015']}},
-        //     // {test: /\.scss$/,loader:'style-loader!css-loader'},
-        //     {test: /\.scss$/,loader:'style-loader!css-loader!sass-loader'},
-        //     　　　 {test: /\.(png|jpg)$/,loader: 'url-loader?limit=8192'}
-        // ]
+    },
+    plugins:[
+        // new HtmlWebpackPlugin(),//生成入口html文件
+        new MiniCssExtractPlugin({
+            filename: "[name].css"
+        })//为抽取出的独立的CSS文件设置配置参数
+    ],
+    optimization:{
+        //对生成的CSS文件进行代码压缩 mode='production'时生效
+        minimizer:[
+            new OptimizeCssAssetsPlugin()
+        ]
     }
 }
