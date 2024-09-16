3b2cf16cec76010e1869000bae32825907e615f2
yingyuan <907280636@qq.com>
2024-09-16 21:16:45 +0800

    升级到webpack5

--- webpack.config.js
+++ webpack.config.js
@@ -49,7 +49,7 @@ module.exports = {
 				exclude: /node_modules/,
 				use: [{
 					loader: 'babel-loader',
-					query: {
+					options: {
 						presets: ['@babel/env', '@babel/react']
 					}//这个是babel的属性
 				}]
